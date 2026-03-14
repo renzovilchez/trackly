1f67b25faee732c3912c61983847e8f9eee8589e
Renzo Vilchez <vilchezalgalobosr@gmail.com>
2026-03-14 10:49:36 -0500

    feat: configurar base de datos SQLite con TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { LinksModule } from './links/links.module';
 import { StatsModule } from './stats/stats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [LinksModule, StatsModule],
+  imports: [LinksModule, StatsModule, TypeOrmModule.forRoot({
+    type: 'better-sqlite3',
+    database: 'trackly.db',
+    entities: [],
+    synchronize: true,
+  })],
   controllers: [],
   providers: [],
 })
