408426af71b2d3b9284e036653b4df38e3c9959d
Renzo Vilchez <vilchezalgalobosr@gmail.com>
2026-03-27 11:23:46 -0500

    feat: registrar módulos links y stats en app principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,21 @@ import { Module } from '@nestjs/common';
 import { LinksModule } from './links/links.module';
 import { StatsModule } from './stats/stats.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Link } from './links/links.entity';
+import { Stat } from './stats/stat.entity';
 
 @Module({
-  imports: [LinksModule, StatsModule, TypeOrmModule.forRoot({
-    type: 'better-sqlite3',
-    database: 'trackly.db',
-    entities: [],
-    synchronize: true,
-  })],
+  imports: [
+    LinksModule,
+    StatsModule,
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'trackly.db',
+      entities: [Link, Stat],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
