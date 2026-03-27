b19b1a677831c719b0b20311d3b3216acfddea79
Renzo Vilchez <vilchezalgalobosr@gmail.com>
2026-03-27 17:33:19 -0500

    chore: migrar de SQLite a PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,34 @@ import { StatsModule } from './stats/stats.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Link } from './links/links.entity';
 import { Stat } from './stats/stat.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     LinksModule,
     StatsModule,
-    TypeOrmModule.forRoot({
-      type: 'better-sqlite3',
-      database: 'trackly.db',
-      entities: [Link, Stat],
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        console.log('=== DATABASE CONFIG ===');
+        console.log('DATABASE_URL:', config.get('DATABASE_URL'));
+        console.log('NODE_ENV:', config.get('NODE_ENV'));
+        console.log('=======================');
+
+        return {
+          type: 'postgres',
+          url: config.get('DATABASE_URL'),
+          ssl:
+            config.get('NODE_ENV') === 'production'
+              ? { rejectUnauthorized: false }
+              : false,
+          entities: [Link, Stat],
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
