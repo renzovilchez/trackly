76e1a2740a6a6e674e76496093981733742e59c2
Renzo Vilchez <vilchezalgalobosr@gmail.com>
2026-03-27 11:17:44 -0500

    feat: crear entidad Link con campos id, url, slug, createdAt y relacion con stats

---
+++ src/links/links.entity.ts
@@ -0,0 +1,26 @@
+import { Stat } from '../stats/stat.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Link {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  url: string;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @OneToMany(() => Stat, (stat) => stat.link)
+  stats: Stat[];
+}
