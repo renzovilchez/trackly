eb24fe75e8f2c7a7ffc7e774805d1023ec324735
Renzo Vilchez <vilchezalgalobosr@gmail.com>
2026-03-27 16:15:39 -0500

    feat: configurar servidor para servir frontend estático

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,10 +14,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.enableCors({
-    origin: '*',
-  });
-  app.getHttpAdapter().getInstance().set('trust proxy', true);
+
+  app.set('trust proxy', true);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
