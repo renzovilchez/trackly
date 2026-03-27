36dd8f0443a26fba27f85c4ce99b2166091a7e9e
Renzo Vilchez <vilchezalgalobosr@gmail.com>
2026-03-27 11:22:52 -0500

    feat: crear módulo de links con imports de TypeORM

--- src/links/links.module.ts
+++ src/links/links.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LinksController } from './links.controller';
 import { LinksService } from './links.service';
+import { RedirectController } from './redirect/redirect.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Link } from './links.entity';
+import { StatsModule } from 'src/stats/stats.module';
 
 @Module({
-  controllers: [LinksController],
-  providers: [LinksService]
+  controllers: [LinksController, RedirectController],
+  providers: [LinksService],
+  imports: [TypeOrmModule.forFeature([Link]), StatsModule],
 })
 export class LinksModule {}
