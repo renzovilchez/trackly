6cbb360cd2d2175a07453c0e7bbc329a3f0206d8
Renzo Vilchez <vilchezalgalobosr@gmail.com>
2026-03-27 11:23:33 -0500

    feat: crear módulo de stats con imports de TypeORM

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { StatsController } from './stats.controller';
 import { StatsService } from './stats.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Stat } from './stat.entity';
+import { Link } from '../links/links.entity';
 
 @Module({
   controllers: [StatsController],
-  providers: [StatsService]
+  providers: [StatsService],
+  imports: [TypeOrmModule.forFeature([Link, Stat])],
+  exports: [StatsService],
 })
 export class StatsModule {}
